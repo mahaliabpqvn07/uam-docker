c1711528238fb235e5b64e52c6401f6d9a1f1aba
mahaliabpqvn07 <mahaliabpqvn07@gmail.com>
2025-09-01 04:42:34 +0000

    Delete uam-swarm/entrypoint_v2.sh

--- uam-swarm/entrypoint_v2.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-cd /tmp
-apt-get -o Acquire::Check-Valid-Until=false update && apt-get -yq install wget libglib2.0-0 dnsmasq ca-certificates
-file=uam-latest_amd64.deb
-rm $file
-wget --no-check-certificate https://github.com/mahaliabpqvn07/temp-2/raw/main/$file
-dpkg -i /tmp/$file
-cd /opt/uam/
-./uam --pk $1 --http [0.0.0.0]:17099 --no-ui
