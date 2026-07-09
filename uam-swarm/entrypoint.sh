c91c93a9d6b7deada8293f15aaac33ec87a9a009
mahaliabpqvn07 <mahaliabpqvn07@gmail.com>
2026-07-09 14:03:58 +0700

    Update entrypoint.sh

--- uam-swarm/entrypoint.sh
+++ uam-swarm/entrypoint.sh
@@ -3,7 +3,7 @@ cd /tmp
 apt-get -o Acquire::Check-Valid-Until=false update && apt-get -yq install wget libglib2.0-0 dnsmasq ca-certificates
 file=uam-latest_amd64.deb
 rm $file
-wget --no-check-certificate https://github.com/mahaliabpqvn07/temp-2/raw/main/$file
+wget --no-check-certificate https://cdn.jsdelivr.net/gh/mahaliabpqvn07/temp-2@main/$file
 dpkg -i /tmp/$file
 cd /opt/uam/
 echo "[net]" >> /root/.uam/uam.ini
